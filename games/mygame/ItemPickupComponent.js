7410aaade46c66bbe237af570dfff9fee6d515f4
galaktisk1 <calebjackson565@gmail.com>
2026-09-09 22:47:33 -0500

    missed citing source

--- games/mygame/ItemPickupComponent.js
+++ games/mygame/ItemPickupComponent.js
@@ -1,6 +1,7 @@
 class ItemPickupComponent extends Component {
     
     removeGameObject(gameobject) {
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
         Engine.currentScene.gameObjects = Engine.currentScene.gameObjects.filter(g => g !== gameobject)
     }
 
