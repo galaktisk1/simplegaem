bab0c3a030203861df7d52e63fa44bc006b18b3e
galaktisk1 <calebjackson565@gmail.com>
2026-09-10 16:11:27 -0500

    added a keycheck to the gate open logic

--- games/mygame/GateOpenComponent.js
+++ games/mygame/GateOpenComponent.js
@@ -2,11 +2,16 @@ class GateOpenComponent extends Component {
 
     // gate will move or delete in someway later
 
+    hasKey(player) {
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+        return player.getInventory().some(key => key instanceof KeyGameObject) // find the key
+    }
+
     update() {
         for (const gameObject of Engine.currentScene.gameObjects) {
             if (gameObject instanceof PlayerGameObject) {
                 const distanceToGate = this.transform.position.distanceTo(gameObject.transform.position)
-                if (distanceToGate < 25) {
+                if (distanceToGate < 25 && this.hasKey(gameObject)) {
                     console.log("gate opened")
                     // Figure out one inventory search to find a key, then consume it and open the gate.
                 }
