92f40f1d70ef058b8c6d74b48681110d5d3c3a6d
galaktisk1 <calebjackson565@gmail.com>
2026-09-09 22:51:38 -0500

    and another

--- games/mygame/ItemPickupComponent.js
+++ games/mygame/ItemPickupComponent.js
@@ -10,6 +10,7 @@ class ItemPickupComponent extends Component {
         for (const gameObject of Engine.currentScene.gameObjects) {
             // check for an instance of the player game object
             // and ignore other game objects
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/instanceof
             if (gameObject instanceof PlayerGameObject) {
                 // mark the distance to the player
                 const distanceToItem = this.transform.position.distanceTo(gameObject.transform.position)
